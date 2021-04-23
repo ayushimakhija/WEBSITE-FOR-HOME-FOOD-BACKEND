f5f483032748a1a1fb38b8b9869b3fd795c60aef
ayushimakhija <ayushi9yn.19@gmail>
2021-04-24 01:55:43 +0530

    created menu controller , menu service and menu respository

--- src/main/java/com/fooddel/Controllers/MenuController.java
+++ src/main/java/com/fooddel/Controllers/MenuController.java
@@ -1,4 +1,42 @@
 package com.fooddel.Controllers;
 
+import com.fooddel.Services.CustomerService;
+import com.fooddel.Services.MenuService;
+import com.fooddel.beans.Customer;
+import com.fooddel.beans.Menu;
+import com.fooddel.repository.CustomerRepository;
+import com.fooddel.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 public class MenuController {
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+     @Autowired
+    private MenuService menuService;
+
+
+    @PostMapping("/Menu")
+    public Menu createMenu(@RequestBody Menu menu)
+    {
+        return menuService.createMenu(menu);
+    }
+
+
+    @GetMapping("/menu/{dishName}")
+    public ResponseEntity<Menu> getMenuByDishName(@PathVariable String dishName) {
+        Menu menu = menuService.getMenuByDishName(dishName);
+        if(menu!=null) {
+            return ResponseEntity.ok(menu);
+        }
+        return null;
+    }
+
+    @PutMapping("menu/{id}")
+    public ResponseEntity<Menu> updateMenu(@PathVariable Integer id, @RequestBody Menu menu) {
+        return menuService.updateMenu(id, menu);
+    }
 }
--- src/main/java/com/fooddel/Services/MenuService.java
+++ src/main/java/com/fooddel/Services/MenuService.java
@@ -1,4 +1,49 @@
 package com.fooddel.Services;
+import com.fooddel.beans.Customer;
+import com.fooddel.beans.Menu;
+import com.fooddel.exceptions.ResourceNotFoundException;
+import com.fooddel.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
 public class MenuService {
+
+    private final MenuRepository menuRepository;
+
+    @Autowired
+    public MenuService(MenuRepository menuRepository) {
+        this.menuRepository = menuRepository;
+    }
+
+
+    public Menu createMenu( Menu menu)
+    {
+        System.out.println(menu.toString());
+        return MenuRepository.save(menu);
+    }
+
+    public ResponseEntity<Menu> updateMenu(Integer id, Menu menuDetails )
+    {
+        Menu menu = menuRepository.findById(id).orElseThrow(()
+                -> new ResourceNotFoundException("dish not exists with id:"+id));
+
+        menu.setDishName(menuDetails.getDishName());
+        menu.setDishImage(menu.getDishImage());
+        menu.setDishPrice(menu.getDishPrice());
+        menu.setDishDescription(menu.getDishDescription());
+
+
+
+        Menu updatedmenu = menuRepository.save(menu);
+        return ResponseEntity.ok(updatedmenu);
+    }
+    public Menu getMenuByDishName(String dishName){
+
+        Menu menu= menuRepository.findByDishName(dishName);
+
+        return menu;  //entity is returned along with the status
+    }
+
+
+
 }
