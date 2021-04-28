8a1f281595c96ab0e8e09d7ec3290192cad056e3
ayushimakhija <ayushi9yn.19@gmail>
2021-04-28 16:08:25 +0530

    Initializing the database tables

--- src/main/java/com/fooddel/Main.java
+++ src/main/java/com/fooddel/Main.java
@@ -1,11 +1,41 @@
 package com.fooddel;
 
+
+import com.fooddel.beans.Customer;
+import com.fooddel.beans.Menu;
+import com.fooddel.beans.foodprovider;
+import com.fooddel.repository.CustomerRepository;
+import com.fooddel.repository.FoodProviderRepository;
+import com.fooddel.repository.MenuRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
+    @Autowired
+    private  CustomerRepository customerRepository;
+    @Autowired
+    private FoodProviderRepository foodproviderRepository;
+    @Autowired
+    private MenuRepository menuRepository;
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+    }
+    @Bean
+    InitializingBean sendDatabase(){
+        return ()->{
+            customerRepository.save(new Customer("ayushi","makhija","7869054251","narsinghpur","ayushi9ym.19@gmail.com","ayushi",false));
+            customerRepository.save(new Customer("guddi","makhija","7869054251","narsinghpur","ayushi9yn.19@gmail.com","ayushi",false));
+            foodproviderRepository.save(new foodprovider("ayushi9yb.19@gmail.com","ayushi","bangalore","7869054251","ayushiCafe"));
+            foodproviderRepository.save(new foodprovider("chirag16@gmail.com","chirag","jabalpur","9875647839","chiragCafe"));
+            menuRepository.save(new Menu("pizza","my fav","chbhbd","230","Monday"));
+            menuRepository.save(new Menu("pasta","my fav","chbhbck","200","sunday"));
+
+        };
     }
 }
+
