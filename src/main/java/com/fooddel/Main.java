a34f3fea70f196c34dbb2af3554fdd710258e5f1
Pavan Kumar Singh <pavansingh9900@gmail.com>
2021-05-16 10:13:30 +0530

    sql repository filled with data

--- src/main/java/com/fooddel/Main.java
+++ src/main/java/com/fooddel/Main.java
@@ -1,10 +1,6 @@
 package com.fooddel;
-import com.fooddel.beans.Customer;
-import com.fooddel.beans.Menu;
-import com.fooddel.beans.foodprovider;
-import com.fooddel.repository.CustomerRepository;
-import com.fooddel.repository.FoodProviderRepository;
-import com.fooddel.repository.MenuRepository;
+import com.fooddel.beans.*;
+import com.fooddel.repository.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -33,10 +29,33 @@ public class Main {
     InitializingBean sendDatabase(){
         return ()->{
             customerRepository.save(new Customer("ayushi","makhija","7869054251","narsinghpur","ayushi9ym.19@gmail.com","ayushi",false));
-            foodproviderRepository.save(new foodprovider("ayushi9yb.19@gmail.com","ayushi","bangalore","7869054251","ayushiCafe"));
-            foodproviderRepository.save(new foodprovider("chirag16@gmail.com","chirag","jabalpur","9875647839","chiragCafe"));
-            menuRepository.save(new Menu("pizza","my fav","chbhbd","230","Monday"));
-            menuRepository.save(new Menu("pasta","my fav","chbhbck","200","sunday"));
+            customerRepository.save(new Customer("pavan","singh","8686886135","shamshabad","pavan@gmail.com","pavan",false));
+            customerRepository.save(new Customer("shabbir","sidhpurwala","1234567890","bangalore","shabbir@gmail.com","shabbir",true));
+            customerRepository.save(new Customer("mehak","dogra","2345678901","bangalore","mehak@gmail.com","mehak",false));
+            customerRepository.save(new Customer("divyansha","agarwal","3456789012","indore","divyansha@gmail.com","divyansha",false));
+
+
+
+            foodproviderRepository.save(new foodprovider("ayushi9yb.19@gmail.com","ayushi","bangalore","ayushi Cafe","7869054251"));
+            foodproviderRepository.save(new foodprovider("pavan@gmail.com","pavan","bangalore","Pavan Tifffins","8686886135"));
+            foodproviderRepository.save(new foodprovider("shabbir@gmail.com","shabbir","bangalore","SS Chicken Biryani","7823456789"));
+            foodproviderRepository.save(new foodprovider("mehak@gmail.com","mehak","hyderabad","Mehak Sweets","9269054251"));
+            foodproviderRepository.save(new foodprovider("chirag16@gmail.com","chirag","jabalpur","chirag Cafe","9875647839"));
+            menuRepository.save(new Menu("pizza","Delicious fresh pan Pizza","chbhbd","230","sunday"));
+            menuRepository.save(new Menu("pasta","Italian pasta","chbhbck","200","monday"));
+            menuRepository.save(new Menu("Veg Dum Biryani","Hyderabadi dum biryani","chbhbd","300","tuesday"));
+            menuRepository.save(new Menu("Butter paneer","Delicious  butter paneer","chbhbck","250","wednesday"));
+            menuRepository.save(new Menu("Aloo Gobi","Delicious aloo gobi","chbhbd","200","thursday"));
+            menuRepository.save(new Menu("Matar Paneer","Delicious matar paneer","chbhbck","250","friday"));
+            menuRepository.save(new Menu("Chole Bature","Delicious chole bature","chbhbck","230","saturday"));
+
+          //  OrdersItemRepository.save(new Orderitems(order_item_id:1,order_iteam_name:"",order_item_price:120,order_id:12 ));
+
+            //OrdersRepository.save(new Orders(totalprice:320));
+
+
+
+
 
         };
     }
